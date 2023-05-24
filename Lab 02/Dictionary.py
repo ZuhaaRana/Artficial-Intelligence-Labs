06ee6fc3aaa81ac164703d45ddac87115ea933f4
ZuhaaRana <zuhaarana@gmail.com>
2023-05-24 11:06:18 +0500

    Lab 02

--- Lab 02/Dictionary.py
+++ Lab 02/Dictionary.py
@@ -12,15 +12,30 @@
 # Girls = {'Arfa':22}
 
 
+
 Dict={'Hannan': 18,'Waleed':12,'Arfa':22,'Ali':25}
 Boys={'Hannan':18, 'Waleed':12,'Ali':25}
+Girls = {'Ayesha':22}
 
-A=0
+B=0
 for D in Dict:
     for B_Dictionary in Boys:
         if(D==B_Dictionary):
-            A+=1
-if(A==len(Boys)):
+            B+=1
+            
+G=0
+for D in Dict:
+    for G_Dictionary in Girls:
+        if(D==G_Dictionary):
+            G+=1
+            
+if(B==len(Boys)):
     print("Boys Dictionary exists in the main dictionary")
 else:
     print("Boys Dictionary doesn't exist in the main dictionary")
+    
+if(G==len(Girls)):
+    print("Girls Dictionary exists in the main dictionary")
+else:
+    print("Girls Dictionary doesn't exist in the main dictionary")
+    
