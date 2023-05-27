0b8bf9c230620ff48b849046935582059c566c33
ZuhaaRana <zuhaarana@gmail.com>
2023-05-27 11:32:17 +0500

    Assignment 1

---
+++ Assignment 1/Task1.py
@@ -0,0 +1,11 @@
+# Task 1
+# Write a python code to print the following sequence of string. “My name is ABC,
+# My roll number is 18F-XXXX, I Like AI Subject :D ”
+# Note: Name and roll number should be yours.
+
+print("My name is Zuha Rana")
+print("My roll number is 19F-0112")
+print("I like AI Subject :D")
+
+
+
