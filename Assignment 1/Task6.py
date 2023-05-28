32844a7c444f77c4d1af11981f03f805f3e89b88
ZuhaaRana <zuhaarana@gmail.com>
2023-05-28 20:38:51 +0500

    Assignment 1

---
+++ Assignment 1/Task6.py
@@ -0,0 +1,11 @@
+
+# Task 6
+# Get number from the user and find the sum of natural numbers till that number.num = int(input("Enter a number : "))
+x = 1
+sum = 0
+while x <= num:
+    sum=sum+x
+    x+=1
+
+print("Sum of all natural numbers till ",num, " is ", sum)
+
