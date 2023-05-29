ed8b81950d27fa276bdff1d4f240c4c9a9b007a2
ZuhaaRana <zuhaarana@gmail.com>
2023-05-30 00:11:48 +0500

    Assignment 1

---
+++ Assignment 1/Task8.py
@@ -0,0 +1,13 @@
+
+# Task 8
+# Use slicing and string concatenation. Str = '''Master has failed more, than the beginner has tried''
+
+str = "Master has failed more, than the beginner has tried"
+x = str[0:24]
+print("STRING SLICING : ")
+print(x)
+y = str[24:51]
+print(y)
+print("CONCATENATION : ")
+z = x+y
+print(z)
