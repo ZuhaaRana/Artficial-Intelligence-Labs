1e9e010f8fa5239bd465ad073a833b02d1e41e84
ZuhaaRana <zuhaarana@gmail.com>
2023-05-31 00:04:31 +0500

    Assignment 1

--- Assignment 1/Task10.py
+++ Assignment 1/Task10.py
@@ -3,3 +3,10 @@
 # list = [ 1, 4, 56, 2, 4 , 12, 6, 89 ,11, 0]
 # Write a program to empty list using while loop and pop() function.
 
+list = [1, 4, 56, 2, 4, 12, 6, 89, 11, 0]
+i=0
+y=(len(list))
+while i<y:
+    list.pop()
+    i+=1
+print("The list after poping all elements : ",list)
