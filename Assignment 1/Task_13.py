bc2af1db00a3507513450dd2bf0a5f0ac2f84116
ZuhaaRana <zuhaarana@gmail.com>
2023-06-02 00:05:56 +0500

    Assignment 1

--- Assignment 1/Task_13.py
+++ Assignment 1/Task_13.py
@@ -1,18 +1,3 @@
 
-# Task 12
-# Write a python code to find if the number is an Armstrong number or not!
-# Note: abcd = an + bn + cn +dn +…., n is the total number of digits.
-
-n = int(input('Enter your number: '))
-pwr = len(str(n))
-sum = 0
-temp = n
-while temp > 0:
-   digit = temp % 10
-   sum += digit ** pwr
-   temp //= 10
-
-if n == sum:
-   print(f"{n} is an Armstrong number")
-else:
-   print(f"{n} is not an Armstrong number")
+#Task 13
+#Write a python code to find the factorial of number using recursion.
