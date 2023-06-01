cf37e982fa60969d902b5b2ea9957adffc8e5bb7
ZuhaaRana <zuhaarana@gmail.com>
2023-06-01 10:24:43 +0500

    Assignment 1

--- Assignment 1/Task_11.py
+++ Assignment 1/Task_11.py
@@ -1,5 +1,10 @@
 
-
+# Task 11
+# marks = {'AI':74,'CN':76,'DS':42,'PS':54}
+# i. Print the sum of marks using for loop.
+# ii. Print keys and values both using for loop.
+# iii. Print max marks and relative subject name.
+# iv. Print mix marks and relative subject name.
 
 marks = {'AI':74,'CN':76,'DS':42,'PS':54}
 sum = 0
