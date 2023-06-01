7b96fdb0c9582ee2bedeb3ab3269ab2310e67150
ZuhaaRana <zuhaarana@gmail.com>
2023-06-02 00:07:11 +0500

    Assignment 1

--- Assignment 1/Task_13.py
+++ Assignment 1/Task_13.py
@@ -1,3 +1,12 @@
 
 #Task 13
 #Write a python code to find the factorial of number using recursion.
+
+def factorial(n):
+    if n==1 or n==0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input('Enter your number: '))
+print(f"Factorial : {factorial(num)}")
