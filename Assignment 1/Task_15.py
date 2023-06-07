3dcedff522b990a5dc44f731a622c61413dd0a8e
ZuhaaRana <zuhaarana@gmail.com>
2023-06-07 17:07:34 +0500

    Assignment 1

--- Assignment 1/Task_15.py
+++ Assignment 1/Task_15.py
@@ -1,24 +1,7 @@
 
-# Task 14
-# Write a python code to make a list. Populate it with random number
-#using random number generator and then find the min and max value from
-#the list. Size of list should be given by user. Note: do not use max 
-#and min built-in function.
-
-from random import randint
-def minmax1 (x):
-    minimum = maximum = x[0]
-    for i in x[1:]:
-        if i < minimum:
-            minimum = i
-        else:
-            if i > maximum: maximum = i
-    return (minimum,maximum)
-
-
-size = int(input('Enter size of the list: '))
-list = [randint(0,100) for i in range(size)]
-
-min, max = minmax1(list)
-
-print(f"List: {list}\nMax: {max}\nMin: {min}")
+# Task 15
+# Write a python code to create a tuple. And do the following tasks.
+# i. Create Tuple = (“john”,”mark”,12,”14”,”orange”,4.5)
+# ii. Then update the Tuple and add a new value 6.5.
+# iii. After updating the value, create a function that shows the count
+# of how many integers, strings and float variables does it have.
