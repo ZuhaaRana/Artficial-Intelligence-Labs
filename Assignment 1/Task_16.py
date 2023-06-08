0f921874a4f26aa97e40c37768125a23f1eeded7
ZuhaaRana <zuhaarana@gmail.com>
2023-06-09 00:54:00 +0500

    Assignment 1

--- Assignment 1/Task_16.py
+++ Assignment 1/Task_16.py
@@ -1,4 +1,15 @@
 
+# Task 16
+# Write a python code to create a dictionary. And do the following tasks.
+# i. Create Dictionary = {“brand”:”Samsung”,”OS- type”:”Oreo”,”color”:”black”,”camera”:”42 megapixels”,”year”:2012}
+# ii. Then add a list in the current dictionary with key = “sizes” and values of random numbers.
+# iii. Then delete the “year” key from the dictionary.
+# iv. Lastly show the dictionary in following order. Use loop to show the dictionary:
+# a. Brand
+# b. Color
+# c. Camera
+# d. OS-type
+# e. Sizes
 
 from random import randint
 dict = {"brand" : "Samsung", "OS-type" : "Oreo", "color" : "black", "camera" : "42 megapixels", "year" : 2012}
