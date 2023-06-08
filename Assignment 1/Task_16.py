9d822dd36673324723c6b2af18962ab2c690bb91
ZuhaaRana <zuhaarana@gmail.com>
2023-06-09 00:50:46 +0500

    Assignment 1

--- Assignment 1/Task_16.py
+++ Assignment 1/Task_16.py
@@ -1,34 +1,19 @@
 
-# Task 15
-# Write a python code to create a tuple. And do the following tasks.
-# i. Create Tuple = (“john”,”mark”,12,”14”,”orange”,4.5)
-# ii. Then update the Tuple and add a new value 6.5.
-# iii. After updating the value, create a function that shows the count
-# of how many integers, strings and float variables does it have.
 
-==Tuple = ("john", "mark", 12, "14", "orange", 4.5)
-print(f"Original tuple: {Tuple}")
-print("Adding..")
-Tuple += (6.5,)
-print(f"Updated tuple: {Tuple}")
+from random import randint
+dict = {"brand" : "Samsung", "OS-type" : "Oreo", "color" : "black", "camera" : "42 megapixels", "year" : 2012}
+print("Original dictionary: {}".format(dict))
 
-def count(t):
-    strs = 0
-    floats = 0
-    ints = 0
-    for i in t:
-        if type(i) == int:
-            ints += 1
-        elif type(i) == str:
-            strs += 1
-        elif type(i) == float:
-            floats += 1
+print("\nAdding sizes..")
+dict['sizes'] = [randint(0, 100) for _ in range(5)]
 
-    return strs, floats, ints
+print("\nUpdated dictionary: {}".format(dict))
 
-strN, floatN, intN = count(Tuple)
-print("Number of string variables: {}".format(strN))
+print("\nDeleting year..")
 
-print("Number of float variables: {}".format(floatN))
+del dict["year"]
 
-print("Number of integer variables: {}".format(intN))
+c = 97
+for k,v in dict.items():
+    print(f"{chr(c)}. {k} : {v}")
+    c += 1
