cd1984bc9a2985093fbdf66afdb176856329d99a
ZuhaaRana <zuhaarana@gmail.com>
2023-06-13 00:25:52 +0500

    Assignment 1

--- Assignment 1/Task_19.py
+++ Assignment 1/Task_19.py
@@ -1,15 +1,6 @@
 
-# Task 18
-# Create a function dsort() to sort a list in descending order, taking
-#a list as argument and returning it. Use the following list and print 
-#it.
-# list = [ 5, 6, 7, 23 ,12 ,3, 3, 4 ,5, 12, 34]
 
-def dsort(l):
-    l.sort(reverse = True)
-    return l
-
-list = [ 5, 6, 7, 23 ,12 ,3, 3, 4 ,5, 12, 34]
-
-print(f"Original array: {list}")
-print(f"Sorted array: {dsort(list[::])}")
+# Task 19
+# Implement Stack and Queue in Python
+# Write separate functions for inserting and deleting and ask the user 
+#to input the values.
