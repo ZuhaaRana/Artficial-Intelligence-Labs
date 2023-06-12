38df1acce9419681143b4baa723dbc1a48d1acc0
ZuhaaRana <zuhaarana@gmail.com>
2023-06-13 00:23:53 +0500

    Assignment 1

--- Assignment 1/Task_18.py
+++ Assignment 1/Task_18.py
@@ -4,3 +4,12 @@
 #a list as argument and returning it. Use the following list and print 
 #it.
 # list = [ 5, 6, 7, 23 ,12 ,3, 3, 4 ,5, 12, 34]
+
+def dsort(l):
+    l.sort(reverse = True)
+    return l
+
+list = [ 5, 6, 7, 23 ,12 ,3, 3, 4 ,5, 12, 34]
+
+print(f"Original array: {list}")
+print(f"Sorted array: {dsort(list[::])}")
