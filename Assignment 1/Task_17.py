b21b330aecd90e5965ca16603123af3a089335c3
ZuhaaRana <zuhaarana@gmail.com>
2023-06-12 23:49:38 +0500

    Assignment 1

--- Assignment 1/Task_17.py
+++ Assignment 1/Task_17.py
@@ -1,3 +1,11 @@
+
+# Task 17
+# Create a function that takes list list1 and list2 as parameter,
+# create a list list3 inside the function and store the sum of list
+# list1, list2 into list3 then return list3.
+# list1 = [11,22,33,44,21,54,67,54,33,222,4]
+# list2 = [3,4,5,32,21,33,66,75,87,97,1]
+
 def sumOfLists(l1, l2):
     l3 = []
     l3.append(sum(l1))
