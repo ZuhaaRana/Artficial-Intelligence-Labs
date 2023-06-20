d215f29ca99cb6e58b10d5ef593e2e760e38eea9
ZuhaaRana <zuhaarana@gmail.com>
2023-06-21 00:32:24 +0500

    Assignment 1

--- Assignment 1/Task_25.py
+++ Assignment 1/Task_25.py
@@ -1,3 +1,41 @@
 
 # Task 25
 # Implement the given graph using adjacency matrix
+
+# Adjacency Matrix representation in Python
+
+
+class Graph(object):
+
+    # Initialize the matrix
+    def __init__(self, size):
+        self.adjMatrix = []
+        for i in range(size):
+            self.adjMatrix.append([0 for i in range(size)])
+        self.size = size
+
+    # Add edges
+    def add_edge(self, v1, v2, weight):
+        #if v1 == v2:
+            #print("Same vertex %d and %d" % (v1, v2))
+        self.adjMatrix[v1][v2] = weight
+        self.adjMatrix[v2][v1] = weight
+
+    #
+    # def __len__(self):
+    #     return self.size
+
+    # Print the matrix
+    def print_matrix(self):
+        print("   " , end=" ")
+        for i in range(0 , 7):
+            print('{:3}'.format(i), " ", end=" "),
+        print()
+        print()
+        i = 0
+        for row in self.adjMatrix:
+            print(i , " " , end =" ")
+            for val in row:
+                print('{:3}'.format(val) , " " , end =" ")
+            print()
+            i+=1
