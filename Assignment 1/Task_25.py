05429f1101cb0ed0f74d314d71100eaad3b7f102
ZuhaaRana <zuhaarana@gmail.com>
2023-06-21 00:33:06 +0500

    Assignment 1

--- Assignment 1/Task_25.py
+++ Assignment 1/Task_25.py
@@ -39,3 +39,23 @@ class Graph(object):
                 print('{:3}'.format(val) , " " , end =" ")
             print()
             i+=1
+
+def main():
+    g = Graph(7)
+    g.add_edge(1, 2, 7)
+    g.add_edge(1, 3, 9)
+    g.add_edge(1, 6, 14)
+
+    g.add_edge(2, 3, 10)
+    g.add_edge(2, 4, 15)
+
+    g.add_edge(3, 4, 11)
+    g.add_edge(3, 6, 2)
+
+    g.add_edge(5, 6, 9)
+    g.add_edge(5, 4, 6)
+
+    g.print_matrix()
+
+if __name__ == '__main__':
+    main()
