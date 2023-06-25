a38f28f3ac844b432cfbc0102312a330c83d60d4
ZuhaaRana <zuhaarana@gmail.com>
2023-06-25 21:07:38 +0500

    Lab 08

--- Lab 08/Task_3.2.py
+++ Lab 08/Task_3.2.py
@@ -5,3 +5,14 @@
 # Find all (a,b) where a ∈ {1,2,3} and b ∈ {1,2,3}  where ‘a’ should be
 # +1 equal to b .
 
+from constraint import *
+p = Problem()
+p.addVariable("a",[1,2,3])
+p.addVariable("b",[1,2,3])
+
+print("Solutions without constraints : ",p.getSolutions())
+p.addConstraint(lambda a,b: a+1 == b,["a","b"])
+
+print("Solutions with constraints : ",p.getSolutions())
+
+
