d444496c55c1ff87aad7c9e86a11b5b8c0274af9
ZuhaaRana <zuhaarana@gmail.com>
2023-06-27 20:45:52 +0500

    Lab 08

--- Lab 08/Task_3.5.py
+++ Lab 08/Task_3.5.py
@@ -1,8 +1,7 @@
 
 # Constraint Satisfaction Problems
 
-# 3.6
-# Define a problem with no solution.
+# 3.5
 
 from constraint import *
 p = Problem()
@@ -18,15 +17,15 @@ p.addVariable("H",['R','B','G'])
 
 p.addConstraint(lambda A,B: A != B,["A","B"])
 p.addConstraint(lambda A,C: A != C,["A","C"])
+p.addConstraint(lambda B,C: B != C,["B","C"])
 p.addConstraint(lambda B,D: B != D,["B","D"])
 p.addConstraint(lambda C,D: C != D,["C","D"])
-p.addConstraint(lambda C,B: C != B,["C","B"])
 p.addConstraint(lambda C,E: C != E,["C","E"])
-p.addConstraint(lambda E,F: F != F,["E","F"])
+p.addConstraint(lambda D,F: D != F,["D","F"])
+p.addConstraint(lambda D,G: D != G,["D","G"])
+p.addConstraint(lambda E,F: E != F,["E","F"])
 p.addConstraint(lambda E,C: E != C,["E","C"])
-p.addConstraint(lambda F,D: F != D,["F","D"])
 p.addConstraint(lambda F,G: F != G,["F","G"])
 
-print("Total Solutions : ",len(p.getSolutions()))
-
+print("Total Solutions with constraints : ",len(p.getSolutions()))
 print("Solutions : ",p.getSolutions())
