7761ba48ceadf5efe51eb934f3dc849aae865f54
ZuhaaRana <zuhaarana@gmail.com>
2023-07-04 00:15:00 +0500

    Lab 12

--- Lab 12/BestFirstSearch.py
+++ Lab 12/BestFirstSearch.py
@@ -1,6 +1,8 @@
+
 # BEST FIRST SEARCH ALGORITHM
 
 # Value of heuristics
+
 A = 40
 B = 32
 C = 25
@@ -9,6 +11,7 @@ E = 19
 F = 17
 H = 10
 G = 0
+
 graph = 
 {
     'A': [('B', B), ('C', C), ('D', D)],
@@ -21,13 +24,11 @@ graph =
     'H': [('E', E), ('G', G)]
 }
 
-
 def heuristic(path):
     value = 0
     for (node, cost) in path:
         value = cost
     return value, path[-1][0]
-
 
 def BEST_FIRST_SEARCH(graph, start, goal):
     visited = []
@@ -50,7 +51,6 @@ def BEST_FIRST_SEARCH(graph, start, goal):
                 new_path = path.copy()
                 new_path.append((node2, cost))
                 queue.append(new_path)
-
 
 sol = BEST_FIRST_SEARCH(graph, 'A', 'G')
 print("Path along with costs is : ", sol)
